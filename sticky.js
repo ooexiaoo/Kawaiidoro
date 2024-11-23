e0f7f428c0f71236d8414116d95f1b59cc006c63
ooexiaoo <vpaherwar@gmail.com>
2024-11-23 07:42:49 +0530

    ads test

---
+++ sticky.js
@@ -0,0 +1,5 @@
+// Add an event listener to the close button
+document.querySelector('.close-btn').addEventListener('click', function() {
+    // Close the sticky bottom ad
+    document.querySelector('.sticky-ad').style.display = 'none';
+  });
